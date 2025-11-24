67fa8e30a9dd0af83856aefc37b9664abf3b31a4
Rich N <blueren75@yahoo.com>
2025-11-24 12:30:58 -0700

    creation photoSets from file of photos

---
+++ data/photoSets.js
@@ -0,0 +1,104 @@
+// Auto-generated photoSets from public assets per pseudocode.md instructions.
+// displayName is the filename without its final extension (precomputed).
+
+export const photoSets = {
+  home: [{ photo: "Collegedale AP.jpg", displayName: "Collegedale AP" }],
+  myAC: [
+    { photo: "myac/A7GCBC.jpg", displayName: "A7GCBC" },
+    { photo: "myac/C182.jpg", displayName: "C182" },
+    { photo: "myac/Cessna140.jpg", displayName: "Cessna140" },
+    { photo: "myac/EAAbiplane.webp", displayName: "EAAbiplane" },
+    { photo: "myac/Harmon.JPG", displayName: "Harmon" },
+    { photo: "myac/KeppernanEZ.jpg", displayName: "KeppernanEZ" },
+    { photo: "myac/Minmax.JPG", displayName: "Minmax" },
+    { photo: "myac/Musketeer.jpeg", displayName: "Musketeer" },
+    { photo: "myac/N43AZ.JPG", displayName: "N43AZ" },
+    { photo: "myac/N590.JPG", displayName: "N590" },
+    { photo: "myac/N60EL EZ.JPG", displayName: "N60EL EZ" },
+    { photo: "myac/N7354M.JPG", displayName: "N7354M" },
+    { photo: "myac/RonWhiteEZ.JPG", displayName: "RonWhiteEZ" },
+    { photo: "myac/RV3B.JPG", displayName: "RV3B" },
+    { photo: "myac/RV8.JPG", displayName: "RV8" },
+    { photo: "myac/TbirdII.jpg", displayName: "TbirdII" },
+    { photo: "myac/Zenith.JPG", displayName: "Zenith" },
+    { photo: "myac/Zenithx.JPG", displayName: "Zenithx" },
+  ],
+  milAC: [
+    { photo: "milac/B57.jpg", displayName: "B57" },
+    { photo: "milac/C131.jpg", displayName: "C131" },
+    { photo: "milac/Cessna01.jpg", displayName: "Cessna01" },
+    { photo: "milac/F104.jpg", displayName: "F104" },
+    { photo: "milac/F86L.jpg", displayName: "F86L" },
+    { photo: "milac/KC135.jpg", displayName: "KC135" },
+    { photo: "milac/O2.jpg", displayName: "O2" },
+    { photo: "milac/SGS126.jpg", displayName: "SGS126" },
+    { photo: "milac/t-28.avif", displayName: "t-28" },
+    { photo: "milac/t-34.avif", displayName: "t-34" },
+    { photo: "milac/t33.jpg", displayName: "t33" },
+    { photo: "milac/t38.jpg", displayName: "t38" },
+    { photo: "milac/U3.jpg", displayName: "U3" },
+  ],
+  harmon: [
+    { photo: "harman/01 BareFuselage.JPG", displayName: "BareFuselage" },
+    {
+      photo: "harman/02 Fuselage before skin.JPG",
+      displayName: "Fuselage before skin",
+    },
+    {
+      photo: "harman/03 Complete Firewall.JPG",
+      displayName: "Complete Firewall",
+    },
+    { photo: "harman/04 Bottom skin.JPG", displayName: "Bottom skin" },
+    {
+      photo: "harman/05 Turtle deck formers.jpg",
+      displayName: "Turtle deck formers",
+    },
+    {
+      photo: "harman/06 Canopy front fairing .JPG",
+      displayName: "Canopy front fairing ",
+    },
+    { photo: "harman/07 Install rudder.JPG", displayName: "Install rudder" },
+    {
+      photo: "harman/08 Fitting the center section.JPG",
+      displayName: "Fitting the center section",
+    },
+    {
+      photo: "harman/09 Level Center Section.JPG",
+      displayName: "Level Center Section",
+    },
+    {
+      photo: "harman/10 Drilling top skin.JPG",
+      displayName: "Drilling top skin",
+    },
+    {
+      photo: "harman/11 Skinning right wing.JPG",
+      displayName: "Skinning right wing",
+    },
+    { photo: "harman/12 Tank bolts.JPG", displayName: "Tank bolts" },
+    { photo: "harman/13 Wireing.JPG", displayName: "Wireing" },
+    {
+      photo: "harman/14 The IO-540 before.JPG",
+      displayName: "The IO-540 before",
+    },
+    {
+      photo: "harman/15 Installed engine.jpg",
+      displayName: "Installed engine",
+    },
+    {
+      photo: "harman/16 Finishing fiberglass.jpg",
+      displayName: "Finishing fiberglass",
+    },
+    { photo: "harman/17 Paint.JPG", displayName: "Paint" },
+    {
+      photo: "harman/18 Instrument panel.JPG",
+      displayName: "Instrument panel",
+    },
+    { photo: "harman/19 Right side.jpg", displayName: "Right side" },
+    {
+      photo: "harman/20 N63AZ and the B17.JPG",
+      displayName: "N63AZ and the B17",
+    },
+  ],
+};
+
+export default photoSets;
---
+++ pseudocode.md
@@ -0,0 +1,45 @@
+# psudo code for allAircraft
+
+<!-- use Vite to create the following react app.
+  the app should have a home page, a my AC page, a milAC page, and a harmon page. app  max width of 2000px and be responsive down to 395 px.
+
+  all pages should be in a single folder called pages. within pages each page will have its own folder named for the page. It will contain a css and a js file for the named page. Use the react css Modules feature.
+
+each page will have a header, a navbar, body and footer
+
+the page headers should read:
+  Home page -- Collegedale International
+  myAC page -- Aircraft I Have Owned
+  milAc page -- Aircraft I Have Flown
+  harmon page  -- The Harmon Build
+
+the navbar, light blue, should span the page and should have with logo, airplane.png, on the left end.  Four buttons, dark blue background with white letters,  on the right: Home, My AC, AF AC, Harmon as titles. active buttons should be red background white letters. the logo and buttons should remain on one line no matter the width of the navbar..
+
+  the home page will have a single photo max width of the app-container. That photo is Collegedale AP.jpg in public folder.  
+  
+   The other pages will have multiple responsive photos each with a max-width 25%.  Clicking on any of these photos will bring up a enlarged display of that photo with its displayName showing  below the photo. The underlying page will be visable thru a blured covering. Clicking on either the enlarged photo or a return button will return to the multiple photo page.  The return button will be in top right corner. It will be red with white letters border-radius of 3px. -->
+
+use the photos in public folder to create a js file with the format of photoSets below.  
+Each entry in the public folder will be used as photo for photoSets.
+
+The name form each photo with the .jpg extention will be stored to displayName as a string
+
+Place photoSets.js in a root level folder called data.
+  
+  export const photoSets = {
+    home: [
+      { photo: "img1.jpg", displayName: "img1" },
+    ],
+    myAC: [
+      { photo: "img1.jpg", displayName: "img1" },
+      { photo: "img2.jpg", displayName: "img2" }
+    ],
+    milAC: [
+      { photo: "img3.jpg", displayName: "img3" },
+      { photo: "img4.jpg", displayName: "img4" }
+    ],
+    harmon: [
+      { photo: "img5.jpg", displayName: "img5" },
+      { photo: "img6.jpg", displayName: "img6" }
+    ]
+  };
